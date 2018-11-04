886b91337623eb94dace221a5d64aa219b1f15ef
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2018-11-04 11:01:07 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// Initialize
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Creating Mock Array
+zooAnimals = {
+  "001" : {
+    name: "Lion",
+    color: "Yellow"
+  },
+  "002" : {
+    name: "Elephant",
+    color: "Gray",
+  }
+}
+
+// Action: Index
+app.get(`/animals`, (req, res) => {
+  res.status(200).send(zooAnimals)
+})
+
+// Action: Create
+app.post(`/animals`, (req, res) => {
+  animalID = req.body.id
+  animal = {
+    name: req.body.name,
+    color: req.body.color
+  }
+  zooAnimals[animalID] = animal
+  res.status(200).send(zooAnimals[animalID])
+})
+
+// Action: Show One
+app.get(`animals/:id`, (req, res) => {
+  animalID = req.body.id
+  res.status(200).send(zooAnimal[animalID])
+})
+
+// Action: Update
+app.put(`animals/:id`, (req, res) => {
+  animalID = req.body.id
+  animal = {
+    name: req.body.name,
+    color: req.body.color
+  }
+  zooAnimals[animalID] = animal
+  res.status(200).send(zooAnimals[animalID])
+})
+
+// Action: Destroy
+app.delete(`animals/:id`, (req, res) => {
+  animalID = req.body.id
+  removedAnimal = zooAnimals[animalID]
+  delete zooAnimals[animalID]
+  res.status(200).send(removedAnimal)
+})
+
+app.listen(3000, () => {
+  console.log("API Working on port 3000")
+})
